248d8c47cee496f3d67679d749488279c097b3cc
Jaaarl <jarl13.work@gmail.com>
2024-09-19 13:16:56 +0800

    adds class & instance method

---
+++ teacher.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+class Teacher
+  attr_accessor :id, :name, :birth_date, :email, :phone_number, :department
+  @@records = []
+
+  def save
+    @@records.prepend(self)
+  end
+
+  def destroy
+    @@records.delete(self)
+  end
+
+  def display
+    puts "ID: #{self.id}, Name: #{self.name}, birth_date: #{self.birth_date}, Email: #{self.email}, Phone Number: #{self.phone_number}, Department: #{self.department}"
+  end
+
+  def self.all
+    return @@records
+  end
+
+  def self.find(id)
+    return @@records.find { |teacher| teacher.id == id }
+  end
+
+  def self.find_by_email(email)
+    return @@records.find { |teacher| teacher.email == email }
+  end
+end
