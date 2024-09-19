77ff9720977c52b3f09da6c6b48e475407ecc2ac
Jaaarl <jarl13.work@gmail.com>
2024-09-19 10:59:06 +0800

    connect to class & adds new_user method

--- main.rb
+++ main.rb
@@ -1,1 +1,19 @@
-puts "Hello World"
+require_relative 'student'
+
+def new_user
+  student = Student.new
+  puts "Enter Name"
+  student.id = Student.all.size + 1
+  student.name = gets.chomp
+  puts "Enter birth date"
+  student.birth_date = gets.chomp
+  puts "Enter Email"
+  student.email = gets.chomp
+  puts "Enter Phone Number"
+  student.phone_number = gets.chomp.to_i
+  student.save
+  puts student.display
+  puts "Student added successfully!"
+end
+
+new_user
