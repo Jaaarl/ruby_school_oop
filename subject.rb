380ad7c315c298121d01d86ce0016d96acd8b4db
Jaaarl <jarl13.work@gmail.com>
2024-09-19 13:04:06 +0800

    adds instance and class method

---
+++ subject.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+class Subject
+  attr_accessor :id, :name
+  @@records = []
+
+  def save
+    @@records.prepend(self)
+  end
+
+  def destroy
+    @@records.delete(self)
+  end
+
+  def display
+    puts "ID: #{self.id}, Name: #{self.name}"
+  end
+
+  def self.all
+    return @@records
+  end
+
+  def self.find(id)
+    return @@records.find { |subject| subject.id == id }
+  end
+end
